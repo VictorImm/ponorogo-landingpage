47e87712127d623c3b6ffba441e23943c1deb2e0
VictorImm <victorimmanuelss@gmail.com>
2023-05-29 19:47:30 +0700

    [UPDATE] navbar

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ window.addEventListener('scroll', function() {
     text.style.marginTop = value * 1.1 + 'px';
 
     if (value > 250) {
-        document.querySelector('.header').classList.add('header-scrolled')
+        document.querySelector('.header').classList.add('header-scrolled');
+        document.querySelector('.header .logo').classList.add('logo-scrolled');
+        document.querySelector('.header ul li .btn-login').classList.add('btn-scrolled');
     } else if (value <= 250) {
-        document.querySelector('.header').classList.remove('header-scrolled')
+        document.querySelector('.header').classList.remove('header-scrolled');
+        document.querySelector('.header .logo').classList.remove('logo-scrolled');
+        document.querySelector('.header ul li .btn-login').classList.remove('btn-scrolled');
     }
 });
