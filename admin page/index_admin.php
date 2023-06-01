cd8d7f32482960c9173c83d2873af799b860cb8d
VictorImm <victorimmanuelss@gmail.com>
2023-06-01 22:36:49 +0700

    [UPDATE] change to .php

---
+++ admin page/index_admin.php
@@ -0,0 +1,84 @@
+<?php
+	include("../config.php");
+	$db = mysqli_query($status, "select * from umkm_db");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="style_admin.css">
+    </head>
+    <body>
+        <div class="table">
+            <div class="table-header">
+                <p>Admin Dashboard</p>
+            </div>
+            <div class="table-section">
+                <table>
+                    <thead>
+                        <th>No.</th>
+                        <th>Nama UMKM</th>
+                        <th>Produk</th>
+                        <th>Owner</th>
+                        <th>Lokasi</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $count = 1;
+                            while($arr = mysqli_fetch_array($db)){
+                                echo "<tr>";
+                                echo "<td>".$count."</td>";
+                                echo "<td>".$arr['umkm']."</td>";
+                                echo "<td>".$arr['produk']."</td>";
+                                echo "<td>".$arr['owner']."</td>";
+                                echo "<td>".$arr['lokasi']."</td>";
+
+                                // Set button
+                                echo "<td>";
+                                ?>
+                                    <form method="post" name="edit" action="API/properties.php">
+
+                                        <?php if($arr['status'] == 1 || $arr['status'] == 2){?>
+                                            <!-- Button trash -->
+                                            <button class="btn-trash" name="action" value="del">
+                                                <ion-icon name="trash"></ion-icon>
+                                            </button>
+
+                                        <?php } else{?>
+                                            <!-- Button yes -->
+                                            <button class="btn-yes" name="action" value="yes">
+                                                <ion-icon name="checkmark-outline"></ion-icon>
+                                            </button>
+
+                                            <!-- Button no -->
+                                            <button class="btn-no" name="action" value="no">
+                                                <ion-icon name="close-outline"></ion-icon>
+                                            </button>
+                                        <?php }?>
+                                        
+                                        <!-- Passing id -->
+                                        <input type="hidden" name="id" value="<?php echo $arr['id_umkm']; ?>"/>
+                                        
+                                    </form>
+                                <?php
+                                echo "</td>";
+
+                                $count++;
+                                echo "</tr>";
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        
+        <!-- Javascript -->
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        <script src="script.js"></script>
+    </body>
+</html>
---
+++ login page/API/login.php
@@ -0,0 +1,33 @@
+<?php
+	include("../../config.php");
+    
+	$db = mysqli_query($status, "select * from user_db");
+
+    $uname = $_POST["uname"];
+    $pw = $_POST["pw"];
+
+    $count = 0;
+
+    // check each item in database
+    while($arr = mysqli_fetch_array($db)){
+
+        // if found
+        if($uname == $arr['nama'] && $pw == $arr['pass']){
+
+            // if the account has admin access
+            if($arr['access'] == 0){
+                header("location: ../../admin page/index_admin.php");
+            }
+            // if the account doesnt has admin access
+            else{
+                header("location: ../../owner page/index_owner.php?id=${arr['id_user']}");
+            }
+            $count = 1;
+        }
+    }
+    
+    // if there is no account or wrong username/pw
+    if($count == 0){
+        header("location: ../login.html");
+    }
+?>
