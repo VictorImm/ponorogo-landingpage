9586a1efb4dec643720365f10e7e9a718dee8d9f
VictorImm <victorimmanuelss@gmail.com>
2023-06-03 01:44:35 +0700

    [UPDATE] minor

--- script.js
+++ script.js
@@ -11,9 +11,19 @@ window.addEventListener('scroll', function() {
         document.querySelector('.header').classList.add('header-scrolled');
         document.querySelector('.header .logo').classList.add('logo-scrolled');
         document.querySelector('.header ul li .btn-login').classList.add('btn-scrolled');
+        
+        document.querySelector('.header ul li .item1').classList.add('scrolled');
+        document.querySelector('.header ul li .item2').classList.add('scrolled');
+        document.querySelector('.header ul li .item3').classList.add('scrolled');
+        document.querySelector('.header ul li .item4').classList.add('scrolled');
     } else if (value <= 250) {
         document.querySelector('.header').classList.remove('header-scrolled');
         document.querySelector('.header .logo').classList.remove('logo-scrolled');
         document.querySelector('.header ul li .btn-login').classList.remove('btn-scrolled');
+
+        document.querySelector('.header ul li .item1').classList.remove('scrolled');
+        document.querySelector('.header ul li .item2').classList.remove('scrolled');
+        document.querySelector('.header ul li .item3').classList.remove('scrolled');
+        document.querySelector('.header ul li .item4').classList.remove('scrolled');
     }
 });
